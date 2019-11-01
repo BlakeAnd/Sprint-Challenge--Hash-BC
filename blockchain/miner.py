f46a8f0db9b168afc3c09a33fd9096d15f0bff8b
BlakeAnd <mblakeandrew@gmail.com>
2019-11-01 10:32:26 -0700

    both hashtable files passing tests, coin miner working on test server

--- blockchain/miner.py
+++ blockchain/miner.py
@@ -1,6 +1,6 @@
 import hashlib
 import requests
-
+import json
 import sys
 
 from uuid import uuid4
@@ -24,8 +24,11 @@ def proof_of_work(last_proof):
     start = timer()
 
     print("Searching for next proof")
+    block_string = json.dumps(last_proof, sort_keys=True)
     proof = 0
     #  TODO: Your code here
+    while valid_proof(block_string, proof) is False:
+        proof += 1
 
     print("Proof found: " + str(proof) + " in " + str(timer() - start))
     return proof
@@ -40,7 +43,11 @@ def valid_proof(last_hash, proof):
     """
 
     # TODO: Your code here!
-    pass
+    guess = f"{last_hash}{proof}".encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+    # print("last ending", last_hash[-6:])
+
+    return guess_hash[:6] == last_hash[-6:]
 
 
 if __name__ == '__main__':
--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -13,11 +13,28 @@ class Ticket:
 
 
 def reconstruct_trip(tickets, length):
+    print("::::::::::::::::::::::::::::")
     hashtable = HashTable(length)
     route = [None] * length
 
     """
     YOUR CODE HERE
     """
+    # source[0] = tickets[0].source
+    for ticket in tickets:
+        # print(ticket.source, ticket.destination)
+        hash_table_insert(hashtable, ticket.destination, ticket.source)
+        if ticket.destination is "NONE":
+            route[length-1] = ticket.source
+    print("r1", route)
+
+    for i in range(length-1, 0, -1):
+        prev_flight = hash_table_retrieve(hashtable, route[i])
+        # print(tickets[i].source, prev_flight)
+        route[i-1] = prev_flight
+    route = route[1:]
+    route.append("NONE")
+    print("r2", route)
+        
 
     return route
